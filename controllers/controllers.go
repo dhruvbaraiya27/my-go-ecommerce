9655deb60bb3b644f9c3a3f24e4907a62135c22e
Dhruv Baraiya <baraiya.d@northeastern.edu>
2025-06-30 17:43:25 -0500

    Update controllers.go

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -7,9 +7,9 @@ import (
 	"net/http"
 	"time"
 
-	"github.com/akhil/ecommerce-yt/database"
-	"github.com/akhil/ecommerce-yt/models"
-	generate "github.com/akhil/ecommerce-yt/tokens"
+	"github.com/dhurvbaraiya27/ecommerce-yt/database"
+	"github.com/dhurvbaraiya27/ecommerce-yt/models"
+	generate "github.com/dhurvbaraiya27/ecommerce-yt/tokens"
 
 	"github.com/gin-gonic/gin"
 	"github.com/go-playground/validator/v10"
